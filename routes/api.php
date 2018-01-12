020711854608f67c1539fdb4cad04615e3ff2f68
ilyas b <ilyas.balluwala@gryphontechnolabs.co.in>
2018-01-12 19:59:28 +0530

    After Passport API Authentication

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('login', 'API\PassportController@login');
+Route::post('register', 'API\PassportController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('get-details', 'API\PassportController@getDetails');
+	Route::post('logout','API\PassportController@logout');
 });
